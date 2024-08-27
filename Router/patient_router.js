b79d085ad583999b564ab686767ab6f97776789e
Ace10079 <minions10karan@gmail.com>
2024-08-27 21:11:06 +0530

    image

--- Router/patient_router.js
+++ Router/patient_router.js
@@ -5,13 +5,17 @@ const path = require('path');
 const PatientController = require('../Controller/PatientController');
 
 // Configure multer for file uploads
-const upload = multer({ 
-    dest: path.join(__dirname, '..', 'uploads') // Ensure the uploads folder path is correct
+const storage = multer.diskStorage({
+    destination: 'img',
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    },
 });
 
 // Define routes
-router.post('/post', upload.single('image'), PatientController.registerPatient);
-router.put('/update', upload.single('image'), PatientController.updatePatient);
+const upload = multer({ storage: storage }).single('img');
+router.post('/post', upload, PatientController.registerPatient);
+router.put('/update', upload, PatientController.updatePatient);
 router.delete('/delete', PatientController.deletePatient);
 router.get('/all', PatientController.getAllPatients);
 router.get('/get', PatientController.getPatientById);
