67a696191714f9c987219d4e6cdcfdd522c5202b
Ace10079 <minions10karan@gmail.com>
2024-08-29 10:49:40 +0530

    hashing

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use environment variable or default to 3000
 const cors = require('cors');
+require('dotenv').config(); // Load environment variables
 
 // Importing routers
 const DentistRouter = require('./Router/dentist_router');
 const AdminRouter = require('./Router/admin_router');
 const SubscriptionRouter = require('./Router/subscription_router');
-const PatientRouter=require('./Router/patient_router');
+const PatientRouter = require('./Router/patient_router');
+
 // Apply CORS middleware before defining routes
 app.use(cors());
 
 // Middleware to parse JSON
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+
+// Serve static files from 'img' directory
+app.use('/images', express.static('img'));
+
 // Define routes
 app.use('/dentist', DentistRouter);
 app.use('/admin', AdminRouter);
 app.use('/subscription', SubscriptionRouter);
-app.use('/patient',PatientRouter)
+app.use('/patient', PatientRouter);
 
 // Basic route
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 // Start the server
